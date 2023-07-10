91047cb79d5f6b48335917d42029b9fdf02c8622
Rafael Oshima <yoshiaki_oshima@outlook.com>
2023-07-10 12:38:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import  './global.css'
+import { Header } from './components/header'
+import { Main } from './components/Main'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+    
+  )
+}
---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+
+
+export function Main() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
